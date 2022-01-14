67017b6089f5ea2bc2c76ffcae0acdfdc6023f45
Ryan Rix <ryan@whatthefuck.computer>
2022-01-14 12:30:50 -0800

    some testcase generation strats

--- gen_cases.py
+++ gen_cases.py
@@ -2,11 +2,51 @@ import models
 import json
 from hypothesis import given, strategies as st
 
-@given(st.builds(models.DataRightsRequest))
+safe_text = st.text(
+    min_size=3,
+    max_size=10,
+    alphabet=st.characters(
+        whitelist_categories=['L', 'P', 'N']
+        #blacklist_categories=['C']
+    )
+)
+simple_text = st.text(
+    alphabet=st.characters(whitelist_categories=['L', 'N', 'P'])
+)
+
+id_strat = st.builds(
+    models.IdentityPayload,
+    iss=safe_text,
+    aud=safe_text,
+    sub=safe_text,
+    name=safe_text,
+    phone_number=simple_text,
+    address=simple_text,
+    power_of_attorney=simple_text,
+)
+
+strat = st.builds(
+    models.DataRightsRequest,
+    identity=id_strat,
+    relationships=st.lists(
+        st.text(
+            min_size=3,
+            max_size=10,
+            alphabet=st.characters(
+                blacklist_categories=['C']
+            )
+        ),
+        min_size=0,
+        max_size=10
+    )
+)
+
+@given(strat)
 def drr_identity_is_str(instance):
        exp = instance.as_json()
        tok = json.loads(exp)['identity']
        print(exp)
        assert(type(tok) == str)
 
-drr_identity_is_str()
+print(strat.example().as_json())
+print(json.dumps(id_strat.example().dict()))
--- shell.nix
+++ shell.nix
@@ -3,7 +3,6 @@
 let
   myAppEnv = pkgs.poetry2nix.mkPoetryEnv {
     projectDir = ./.;
-    # overrides = import ./poetry-overrides.nix { inherit pkgs; };
     overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
       typing-extensions = super.typing-extensions.overridePythonAttrs(old: {
         format = "flit";
@@ -13,8 +12,6 @@ let
 in pkgs.mkShell {
   packages = [
     myAppEnv
-    pkgs.pandoc
     pkgs.poetry
-    # inotify-tools
   ];
 }
